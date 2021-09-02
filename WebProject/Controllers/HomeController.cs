45d1c156416da79adab242ca090df7780590b8e0
Ling <1427917847@qq.com>
2021-09-02 19:45:10 +0800

    Add project files.

---
+++ WebProject/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebProject.Models;
+
+namespace WebProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        DBEntities db = new DBEntities();
+        public ActionResult CountryMedalList()
+        {
+            //Set Country No
+            int No = 1;
+
+            var coutries = db.Countries.ToList().OrderByDescending(a => a.Athletes.Sum(x => x.MedalLists.Count(m => m.MedalType == 1))).ToList();
+            coutries.ForEach(a =>
+            {
+                a.No = No++;
+            });
+
+
+            return View(coutries);
+        }
+
+        public ActionResult NewsDetail(int nid)
+        {
+            
+            return View(db.News.Find(nid));
+        }
+
+
+
+
+    }
+}
---
+++ WebProject/Models/CountryExpand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebProject.Models
+{
+    public partial class Country
+    {
+        public int No { get; set; }
+        public int GoldNumber => Athletes.Sum(a=>a.MedalLists.Count(x=>x.MedalType == 1));
+        public int SilverNumber => Athletes.Sum(a=>a.MedalLists.Count(x=>x.MedalType == 2));
+        public int BronzeNumber => Athletes.Sum(a=>a.MedalLists.Count(x=>x.MedalType == 3));
+        public int TotalNumber => Athletes.Sum(a => a.MedalLists.Count());
+    }
+}
